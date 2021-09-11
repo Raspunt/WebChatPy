604a664577aa070226b39318524e3ac9f271629f
Raspunt <m_ryabinin@inbox.ru>
2021-09-11 11:12:14 +0600

    Login_register_page

--- users/urls.py
+++ users/urls.py
@@ -1,9 +1,10 @@
+from django.contrib import auth
 from django.urls import path 
-
+from django.contrib.auth import views as auth_view
 from .  import views
 
 urlpatterns = [
-    path("login/",views.login,name="startPageUrl"),
+    path("login/",auth_view.LoginView.as_view(template_name = "users/login.html"),name="startPageUrl"),
     path("reg/",views.reg,name="startPageUrl"),
 
 ]
--- users/views.py
+++ users/views.py
@@ -1,15 +1,39 @@
 from django.http import request
-from django.shortcuts import render
+from django.shortcuts import redirect, render
+from django.contrib.auth.models import User
 
-
-
-
-def login(request):
-    return render(request,'users/login.html')
 
 def reg(request):
     
-    userData = request.POST
-    print(userData)
+    error = ""
+    if request.method == "POST": # если страница отправляет POST то
 
-    return render(request,'users/reg.html')
+
+        #  Берем информацию из POST
+        userName = request.POST.get("UserName") 
+        Email = request.POST.get("Email")
+        password_1 = request.POST.get("password_1")
+        password_2 = request.POST.get("password_2")
+
+        # Проверяем пароль 
+        if password_1 != password_2 :
+            error = "Password mismatch"
+            print("пароли не совпадают")
+        else:
+            print("пароли совпадают")    
+
+            # создаем пользователя без пароля
+            user = User(username=userName,email=Email)
+
+            # set_password  зашифровывает пароль сверху если вставить шифровки не будет
+            user.set_password(password_1)
+            user.save()
+
+            redirect("/")
+
+
+
+    return render(request,'users/reg.html',
+    {
+        "error":error
+    })
